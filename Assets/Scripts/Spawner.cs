9d8e269568a27f2b67e3d54afb4f6767fd2b66f8
Androix <androix77@gmail.com>
2019-07-09 23:41:50 +0500

    Добавлен спаунер противников

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private int spawnRate;
+    [SerializeField]
+    private GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 1 / spawnRate, 1 / spawnRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        Instantiate(enemy, this.transform.position, Quaternion.identity);
+    }
+}
